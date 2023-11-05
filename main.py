2441bc72797517fc4975b0fb62d14642176f6228
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2023-11-05 12:03:03 -0800

    update code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -5,161 +6,118 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
-from transformers import pipeline
-from datasets import concatenate_datasets, load_dataset_builder, get_dataset_config_names, load_dataset
+from datasets import load_dataset
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
 
+# Load the Social Bias Frames dataset
+social_bias_frames = load_dataset("social_bias_frames")
 
-ds_builder = load_dataset_builder("amazon_reviews_multi")
-print(ds_builder.info.description)
-configs = get_dataset_config_names("amazon_reviews_multi")
-print(configs)
+# Check if links and emojis in text
+def has_links(text):
+    # Define a regular expression for URLs
+    url_pattern = r'https?://\S+|www\.\S+'
+    return int(bool(re.search(url_pattern, text)))
 
-# train
-trainen = load_dataset("amazon_reviews_multi", "en", split="train")
-trainde = load_dataset("amazon_reviews_multi", "de", split="train")
-tr = concatenate_datasets([trainen, trainde])
+def has_emojis(text):
+    # Define a regular expression for emojis
+    emoji_pattern = r'[\U00010000-\U0010ffff]'
+    return int(bool(re.search(emoji_pattern, text)))
 
-#test
-testen = load_dataset("amazon_reviews_multi", "en", split="test")
-testde = load_dataset("amazon_reviews_multi", "de", split="test")
-te = concatenate_datasets([testen, testde])
+# Apply the custom functions to the dataset
+def apply_custom_fields(data_field):
+    data_field['Has links'] = has_links(data_field['post'])
+    data_field['HasEmojis Y/N'] = has_emojis(data_field['post'])
+    return data_field
 
-
-def count_words(text):
-    # Split the text by spaces to count words
-    words = text.split()
-    return len(words)
-
-
-def calculate_word_counts(dat, col):
-    dat[f"{col}_word_count"] = count_words(dat[col])
-    return dat
-
-
-def char_counts(dat, col):
-    dat[f"{col}_char_count"] = len(dat[col])
-    return dat
-
-
-def calculate_average_word_length(example, column_name):
-    words = example[column_name].split()
-    total_word_length = sum(len(word) for word in words)
-    example[f"{column_name}_avg_word_length"] = total_word_length / len(words) if len(words) > 0 else 0
-    return example
-
-
-def calculate_unique_word_count(dat, col):
-    words = dat[col].split()
-    unique_words = set(words)
-    dat[f"{col}_unique_word_count"] = len(unique_words)
-    return dat
-
-
-def calculate_uppercase_letter_count(dat, col):
-    uppercase_count = sum(1 for letter in dat[col] if letter.isupper())
-    dat[f"{col}_uppercase_letter_count"] = uppercase_count
-    return dat
-
-
-def calculate_numeric_character_count(dat, col):
-    numeric_count = sum(1 for char in dat[col] if char.isnumeric())
-    dat[f"{col}_numeric_char_count"] = numeric_count
-    return dat
-
-
-def apply_calculate_counts(dat):
-    dat = calculate_word_counts(dat, "review_body")
-    dat = calculate_word_counts(dat, "review_title")
-    dat = char_counts(dat, "review_body")
-    dat = char_counts(dat, "review_title")
-    dat = calculate_average_word_length(dat, "review_body")
-    dat = calculate_average_word_length(dat, "review_title")
-    dat = calculate_unique_word_count(dat, "review_body")
-    dat = calculate_unique_word_count(dat, "review_title")
-    dat = calculate_uppercase_letter_count(dat, "review_body")
-    dat = calculate_uppercase_letter_count(dat, "review_title")
-    return dat
-
-
-tr = tr.map(apply_calculate_counts)
-te = te.map(apply_calculate_counts)
-
-labels_tr = tr["stars"]
-data_tr = tr.remove_columns("stars")
-labels_te = te["stars"]
-data_te = te.remove_columns("stars")
-
-# Combine text from the first 7 columns into a single text field
-data_tr = data_tr.to_pandas()
-data_te = data_te.to_pandas()
-def combine_text_columns(row):
-    return ' '.join(row)
-
-data_tr['combined_text'] = data_tr.iloc[:, :7].apply(combine_text_columns, axis=1)
-data_te['combined_text'] = data_te.iloc[:, :7].apply(combine_text_columns, axis=1)
-
-# Use the last 13 integer columns as additional features
-train_features = data_tr.iloc[:, 7:17]
-test_features = data_te.iloc[:, 7:17]
-
-# Step 2: Preprocess the text data using TF-IDF
-tfidf_vectorizer = TfidfVectorizer(min_df=100, token_pattern=r'[a-zA-Z]+')
-X_train = tfidf_vectorizer.fit_transform(data_tr['combined_text'])
-X_test = tfidf_vectorizer.transform(data_te['combined_text'])
-
-print(X_train.shape)  # Check the shape of X_train
-print(train_features.shape)  # Check the shape of train_features
-
-# Combine text features and integer features
-import scipy.sparse as sp
-from scipy.sparse import hstack
-
-X_combined_train = hstack([X_train, train_features.values], format='csr')
-X_combined_test = hstack([X_test, test_features.values], format='csr')
-
-# Step 4: Prepare labels (assuming your target column is in a DataFrame)
-y_train = labels_tr
-y_test = labels_te
-
-# Step 5: Train the Decision Tree Classifier
-clf = DecisionTreeClassifier()
-clf.fit(X_combined_train, y_train)
-
-# Step 6: Make Predictions on the test set
-y_pred = clf.predict(X_combined_test)
-
-# Step 7: Evaluate the Model
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-
-print(f"Accuracy: {accuracy}")
-print("\nClassification Report:")
-print(report)
+social_bias_frames = social_bias_frames.map(apply_custom_fields)
 
 # Convert the dataset to a pandas DataFrame
-df = tr.to_pandas()
+df = social_bias_frames['train'].to_pandas()
 
 # Print the DataFrame
 print(df)
 
-# Step 8: Train the Naive Bayes Classifier
+# Assuming 'offensiveYN' is the target column and 'post' is the text column you want to use
+labels = df['offensiveYN']
+data = df.drop(columns=['offensiveYN'])
+
+# Combine text from the 'post' column into a single text field
+data['combined_text'] = data['post']
+
+# Preprocess the text data using TF-IDF
+tfidf_vectorizer = TfidfVectorizer(min_df=100, token_pattern=r'[a-zA-Z]+')
+X = tfidf_vectorizer.fit_transform(data['combined_text'])
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+
+# Train the Decision Tree Classifier
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+
+# Make Predictions on the test set
+y_pred = clf.predict(X_test)
+
+# Evaluate the Model
+accuracy_dt = accuracy_score(y_test, y_pred)
+report_dt = classification_report(y_test, y_pred, zero_division=1)
+
+print(f"Decision Tree Results:")
+print(f"Accuracy: {accuracy_dt}")
+print("\nDecision Tree Classification Report:")
+print(report_dt)
+
+# Train the Naive Bayes Classifier
 nb_clf = MultinomialNB()
-nb_clf.fit(X_combined_train, y_train)
+nb_clf.fit(X_train, y_train)
 
-# Step 9: Make Predictions on the test set with Naive Bayes
-y_pred_nb = nb_clf.predict(X_combined_test)
+# Make Predictions on the test set with Naive Bayes
+y_pred_nb = nb_clf.predict(X_test)
 
-# Step 10: Evaluate the Naive Bayes Model
+# Evaluate the Naive Bayes Model
 accuracy_nb = accuracy_score(y_test, y_pred_nb)
-report_nb = classification_report(y_test, y_pred_nb)
+report_nb = classification_report(y_test, y_pred_nb, zero_division=1)
 
-print("\nNaive Bayes Results")
+print("\nNaive Bayes Results:")
 print(f"Accuracy: {accuracy_nb}")
-print("\nClassification Report:")
+print("\nNaive Bayes Classification Report:")
 print(report_nb)
 
-# Convert the dataset to a pandas DataFrame
-df = tr.to_pandas()
+# Standardize the features before feeding them into MLP
+scaler = StandardScaler(with_mean=False)  # with_mean=False to support sparse input
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-# Print the DataFrame
-print(df)
+# Train the Multi-layer Perceptron Classifier
+mlp_clf = MLPClassifier(random_state=1, max_iter=300)
+mlp_clf.fit(X_train_scaled, y_train)
+
+# Make Predictions on the test set with MLP
+y_pred_mlp = mlp_clf.predict(X_test_scaled)
+
+# Evaluate the MLP Model
+accuracy_mlp = accuracy_score(y_test, y_pred_mlp)
+report_mlp = classification_report(y_test, y_pred_mlp, zero_division=1)
+
+print("\nMulti-layer Perceptron Results")
+print(f"Accuracy: {accuracy_mlp}")
+print("\nMulti-layer Perceptron Classification Report:")
+print(report_mlp)
+
+# Train the Logistic Regression Classifier
+log_reg = LogisticRegression(max_iter=1000)
+log_reg.fit(X_train, y_train)
+
+# Make Predictions on the test set with Logistic Regression
+y_pred_lr = log_reg.predict(X_test)
+
+# Evaluate the Logistic Regression Model
+accuracy_lr = accuracy_score(y_test, y_pred_lr)
+report_lr = classification_report(y_test, y_pred_lr, zero_division=1)
+
+print("\nLogistic Regression Results")
+print(f"Accuracy: {accuracy_lr}")
+print("\nLogistic Regression Classification Report:")
+print(report_lr)
