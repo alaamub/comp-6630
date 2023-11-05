e685f5fe75742e92138280074e9ecb347ec9aa31
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2023-11-05 12:19:02 -0800

    update

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import re
 import numpy as np
 import pandas as pd
+import re
+from scipy.sparse import csr_matrix, hstack
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
-from datasets import load_dataset
-from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from datasets import concatenate_datasets, load_dataset_builder, get_dataset_config_names, load_dataset
 
-# Load the Social Bias Frames dataset
-social_bias_frames = load_dataset("social_bias_frames")
+# Load Social Bias Frames dataset
+ds_builder = load_dataset_builder("social_bias_frames")
+print(ds_builder.info.description)
+configs = get_dataset_config_names("social_bias_frames")
+print(configs)
+
+# train
+train_data = load_dataset("social_bias_frames", split="train")
+#test
+test_data = load_dataset("social_bias_frames", split="test")
 
 # Check if links and emojis in text
 def has_links(text):
@@ -26,59 +35,80 @@ def has_emojis(text):
     return int(bool(re.search(emoji_pattern, text)))
 
 # Apply the custom functions to the dataset
-def apply_custom_fields(data_field):
-    data_field['Has links'] = has_links(data_field['post'])
-    data_field['HasEmojis Y/N'] = has_emojis(data_field['post'])
-    return data_field
+def apply_custom_features(dataset):
+    dataset = dataset.map(lambda x: {"HasLinksYN": 'Y' if has_links(x['post']) else 'N'})
+    dataset = dataset.map(lambda x: {"HasEmojisYN": 'Y' if has_emojis(x['post']) else 'N'})
+    return dataset
 
-social_bias_frames = social_bias_frames.map(apply_custom_fields)
+train_data = apply_custom_features(train_data)
+test_data = apply_custom_features(test_data)
+
+# Separate the features and labels for training and testing data
+labels_tr = train_data["offensiveYN"]
+data_tr = train_data.remove_columns("offensiveYN")
+labels_te = test_data["offensiveYN"]
+data_te = test_data.remove_columns("offensiveYN")
 
 # Convert the dataset to a pandas DataFrame
-df = social_bias_frames['train'].to_pandas()
+data_tr = data_tr.to_pandas()
+data_te = data_te.to_pandas()
 
-# Print the DataFrame
-print(df)
+# Step 2: Preprocess the text data using TF-IDF
+tfidf_vectorizer = TfidfVectorizer(min_df=10, token_pattern=r'[a-zA-Z]+')
+X_train = tfidf_vectorizer.fit_transform(data_tr['post'])
+X_test = tfidf_vectorizer.transform(data_te['post'])
 
-# Assuming 'offensiveYN' is the target column and 'post' is the text column you want to use
-labels = df['offensiveYN']
-data = df.drop(columns=['offensiveYN'])
+print(X_train.shape)  # Check the shape of X_train
 
-# Combine text from the 'post' column into a single text field
-data['combined_text'] = data['post']
+# Combine text features and additional features
+data_tr['HasLinksYN'] = data_tr['HasLinksYN'].map({'N': 0, 'Y': 1})
+data_tr['HasEmojisYN'] = data_tr['HasEmojisYN'].map({'N': 0, 'Y': 1})
 
-# Preprocess the text data using TF-IDF
-tfidf_vectorizer = TfidfVectorizer(min_df=100, token_pattern=r'[a-zA-Z]+')
-X = tfidf_vectorizer.fit_transform(data['combined_text'])
+data_te['HasLinksYN'] = data_te['HasLinksYN'].map({'N': 0, 'Y': 1})
+data_te['HasEmojisYN'] = data_te['HasEmojisYN'].map({'N': 0, 'Y': 1})
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+# Select only the numeric columns for converting to sparse matrix
+numeric_columns_tr = ['HasLinksYN', 'HasEmojisYN'] 
+numeric_columns_te = ['HasLinksYN', 'HasEmojisYN']
 
-# Train the Decision Tree Classifier
+# Convert additional features to sparse matrix
+data_tr_sparse = csr_matrix(data_tr[numeric_columns_tr].values)
+data_te_sparse = csr_matrix(data_te[numeric_columns_te].values)
+
+# Combine text features and additional features
+X_combined_train = hstack([X_train, data_tr_sparse], format='csr')
+X_combined_test = hstack([X_test, data_te_sparse], format='csr')
+
+# Step 4: Prepare labels
+y_train = labels_tr
+y_test = labels_te
+
+# Step 5: Train the Decision Tree Classifier
 clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
+clf.fit(X_combined_train, y_train)
 
-# Make Predictions on the test set
-y_pred = clf.predict(X_test)
+# Step 6: Make Predictions on the test set
+y_pred = clf.predict(X_combined_test)
 
-# Evaluate the Model
+# Step 7: Evaluate the Model
 accuracy_dt = accuracy_score(y_test, y_pred)
-report_dt = classification_report(y_test, y_pred, zero_division=1)
+report_dt = classification_report(y_test, y_pred, zero_division=0)
 
 print(f"Decision Tree Results:")
 print(f"Accuracy: {accuracy_dt}")
 print("\nDecision Tree Classification Report:")
 print(report_dt)
 
-# Train the Naive Bayes Classifier
+# Step 8: Train the Naive Bayes Classifier
 nb_clf = MultinomialNB()
-nb_clf.fit(X_train, y_train)
+nb_clf.fit(X_combined_train, y_train)
 
-# Make Predictions on the test set with Naive Bayes
-y_pred_nb = nb_clf.predict(X_test)
+# Step 9: Make Predictions on the test set with Naive Bayes
+y_pred_nb = nb_clf.predict(X_combined_test)
 
-# Evaluate the Naive Bayes Model
+# Step 10: Evaluate the Naive Bayes Model
 accuracy_nb = accuracy_score(y_test, y_pred_nb)
-report_nb = classification_report(y_test, y_pred_nb, zero_division=1)
+report_nb = classification_report(y_test, y_pred_nb, zero_division=0)
 
 print("\nNaive Bayes Results:")
 print(f"Accuracy: {accuracy_nb}")
@@ -86,7 +116,7 @@ print("\nNaive Bayes Classification Report:")
 print(report_nb)
 
 # Standardize the features before feeding them into MLP
-scaler = StandardScaler(with_mean=False)  # with_mean=False to support sparse input
+scaler = StandardScaler(with_mean=False)
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
@@ -99,7 +129,7 @@ y_pred_mlp = mlp_clf.predict(X_test_scaled)
 
 # Evaluate the MLP Model
 accuracy_mlp = accuracy_score(y_test, y_pred_mlp)
-report_mlp = classification_report(y_test, y_pred_mlp, zero_division=1)
+report_mlp = classification_report(y_test, y_pred_mlp, zero_division=0)
 
 print("\nMulti-layer Perceptron Results")
 print(f"Accuracy: {accuracy_mlp}")
@@ -115,7 +145,7 @@ y_pred_lr = log_reg.predict(X_test)
 
 # Evaluate the Logistic Regression Model
 accuracy_lr = accuracy_score(y_test, y_pred_lr)
-report_lr = classification_report(y_test, y_pred_lr, zero_division=1)
+report_lr = classification_report(y_test, y_pred_lr, zero_division=0)
 
 print("\nLogistic Regression Results")
 print(f"Accuracy: {accuracy_lr}")
